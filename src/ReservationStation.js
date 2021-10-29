d7a281def1a4a99c9831ffd5735103a6f03e6db8
Leonardo Akira Shimabukuro <leonardoashimabukuro@gmail.com>
2021-10-29 17:57:52 -0300

    Adiciona Register e ReservationStation

---
+++ src/ReservationStation.js
@@ -0,0 +1,11 @@
+class ReservationStation {
+    constructor() {
+        this.busy = false; //boolean
+        this.op; //string
+        this.vj; //string
+        this.vk; //string
+        this.Qj; //referencia ReservationStation
+        this.Qk; //referencia ReservationStation
+        this.tempo = 0;
+    }
+}
