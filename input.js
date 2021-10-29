ce70fc55ecc57fc4ebc87bdbef25f86bb2b593b4
EduardoTA <EduardoTA@live.com>
2021-10-29 16:41:51 -0300

    Commit inicial interface

---
+++ input.js
@@ -0,0 +1,20 @@
+
+
+document.getElementById("applyNInst").onclick =
+() => {
+    let instInput = document.getElementById("instInput");
+    const nInst = instInput.childNodes.length-1;
+    const newNInst = document.getElementById("NInst").value;
+    
+    if(nInst < newNInst){
+        for(let i=nInst; i < newNInst; i++){
+            let tr = document.createElement("tr");
+            let select = document.createElement("select");
+            let options = [
+
+            ];
+            tr.appendChild()
+            instInput.appendChild(tr);
+        }
+    }
+};
