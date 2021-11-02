4c5a85c11505873e64d15580adbb8219632e6607
Thiago Yukio <thiyukio@gmail.com>
2021-11-02 01:47:14 -0300

    input instrucoes

--- src/utils.js
+++ src/utils.js
@@ -1,37 +1,46 @@
-import Inst from 'inst.js'
+document.getElementById("applyBTN").onclick = parseInput;
 
-export function parseInput(textInput) {
+function parseInput() {
+    textInput = document.getElementById("instrucoes").value
     let instructions = []
-    
     let linhas = textInput.split("\n");
-    for (const linha of linhas) {
+    for (let linha of linhas) {
         linha = linha.trim()
         linha = linha.split(" ")
         let opcode = linha[0];
         switch (opcode) {
-            case ('add', 'sub', 'sll', 'slt', 'sltu', 'xor', 'srl', 'sra', 'or', 'and'): // Tipo R
-                let [rd, rs1, rs2]  = linha[1].split(/[,()]+/);
-                instructions.push(new Inst({op:opcode, rd:rd, rs1:rs1, rs2:rs2, issueTime:1, execTime:3, wbTime:1, regFile:regFile}))
-            case ('addi', 'slti', 'sltiu', 'xori', 'ori', 'andi', 'slii', 'srli', 'srai'): // Tipo I
-                let [rd, rs1, imm]  = linha[1].split(/[,()]+/);
-                instructions.push(new Inst({op:opcode, rd:rd, rs1:rs1, imm:imm, issueTime:1, execTime:3, wbTime:1, regFile:regFile}))
-            case ('sb', 'sh', 'sw'): // Tipo S
-                let [rs2, offset, rs1]  = linha[1].split(/[,()]+/);
-                instructions.push(new Inst({op:opcode, offset:offset, rs1:rs1, rs2:rs2, issueTime:1, execTime:3, wbTime:1, regFile:regFile}))
-            case ('beq', 'bne', 'blt', 'bge', 'bltu', 'bgeu'): // Tipo SB
-                let [rs1, rs2, offset]  = linha[1].split(/[,()]+/);
-                instructions.push(new Inst({op:opcode, offset:offset, rs1:rs1, rs2:rs2, issueTime:1, execTime:3, wbTime:1, regFile:regFile}))
-            case ('lui'): // Tipo U
-                let [rd, imm]  = linha[1].split(/[,()]+/);
-                instructions.push(new Inst({op:opcode, rd:rd, imm:imm, issueTime:1, execTime:3, wbTime:1, regFile:regFile}))
-            case ('jal', 'auipc'): // Tipo UJ
-                let [rd, offset]  = linha[1].split(/[,()]+/);
-                instructions.push(new Inst({op:opcode, rd:rd, offset:offset, issueTime:1, execTime:3, wbTime:1, regFile:regFile}))
-            case ('jalr'):
-                let [rd, rs1, offset]  = linha[1].split(/[,()]+/);
-                instructions.push(new Inst({op:opcode, rd:rd, rs1:rs1, offset:offset, issueTime:1, execTime:3, wbTime:1, regFile:regFile}))
+            case 'add': case  'sub': case  'sll': case 'slt': case 'sltu': case 'xor': case 'srl': case 'sra': case 'or': case 'and': // Tipo R
+                var [rd, rs1, rs2]  = linha[1].split(/[,()\n\s]+/);
+                console.log([rd,rs1,rs2])
+                instructions.push(new Inst({op:opcode, rd:rd, rs1:rs1, rs2:rs2, issueTime:1, execTime:3, wbTime:1, regFile:regFile}));
+                break;
+            case 'addi': case 'slti': case 'sltiu': case 'xori': case 'ori': case 'andi': case 'slii': case 'srli': case 'srai': // Tipo I
+                var [rd, rs1, imm]  = linha[1].split(/[,()]+/);
+                instructions.push(new Inst({op:opcode, rd:rd, rs1:rs1, imm:imm, issueTime:1, execTime:3, wbTime:1, regFile:regFile}));
+                break;
+            case 'sb': case 'sh': case 'sw': // Tipo S
+                var [rs2, offset, rs1]  = linha[1].split(/[,()]+/);
+                instructions.push(new Inst({op:opcode, offset:offset, rs1:rs1, rs2:rs2, issueTime:1, execTime:3, wbTime:1, regFile:regFile}));
+                break;
+            case 'beq': case 'bne': case 'blt': case 'bge': case 'bltu': case 'bgeu': // Tipo SB
+                var [rs1, rs2, offset]  = linha[1].split(/[,()]+/);
+                instructions.push(new Inst({op:opcode, offset:offset, rs1:rs1, rs2:rs2, issueTime:1, execTime:3, wbTime:1, regFile:regFile}));
+                break;
+            case 'lui': // Tipo U
+                var [rd, imm]  = linha[1].split(/[,()]+/);
+                instructions.push(new Inst({op:opcode, rd:rd, imm:imm, issueTime:1, execTime:3, wbTime:1, regFile:regFile}));
+                break;
+            case 'jal': case 'auipc': // Tipo UJ
+                var [rd, offset]  = linha[1].split(/[,()]+/);
+                instructions.push(new Inst({op:opcode, rd:rd, offset:offset, issueTime:1, execTime:3, wbTime:1, regFile:regFile}));
+                break;
+            case 'jalr':
+                var [rd, rs1, offset]  = linha[1].split(/[,()]+/);
+                instructions.push(new Inst({op:opcode, rd:rd, rs1:rs1, offset:offset, issueTime:1, execTime:3, wbTime:1, regFile:regFile}));
+                break;
+            }
         }
-    }
+    console.log(instructions)
     return instructions;
 }
         // let i = undefined;
